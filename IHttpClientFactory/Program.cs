ff1ea940d38e0e88accdcf80cb668322796680b2
Paulo Pitte <pauloanalista@outlook.com>
2021-08-03 11:37:30 -0300

    Adicionar arquivos de projeto.

---
+++ IHttpClientFactory/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+
+namespace IHttpClientFactoryTest
+{
+    internal sealed class Program
+    {
+        public static int Main(string[] args)
+        {
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(@"c:\log\binance.log")
+                .CreateLogger();
+
+
+            try
+            {
+                Log.Information("Iniciando Processamento");
+                CreateHost(args).Build().Run();
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Falha: Host finalizado inesperadamente.");
+                return 1;                
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+           
+            
+        }
+
+
+        public static IHostBuilder CreateHost(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostcontext, services) =>            
+            {
+                services.AddHttpClient<WorkerProcess>(nameof(WorkerProcess), options =>
+                {
+                    options.BaseAddress = new Uri("https://api.binance.com/");
+                });
+
+                // Habilita o serviço backgroud
+                services.AddHostedService<WorkerProcess>();
+
+            })
+            // Habilita o Uso do Seq
+            .UseSerilog();
+        
+        
+    }
+}
---
+++ IHttpClientFactory/WorkerProcess.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+namespace IHttpClientFactoryTest
+{
+    /// <summary>
+    /// Implementa classe Abstrata para serviços rodando em 2 plano
+    /// </summary>
+    class WorkerProcess : BackgroundService
+    {
+        private readonly ILogger<WorkerProcess> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public WorkerProcess(ILogger<WorkerProcess> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (false.Equals(stoppingToken.IsCancellationRequested))
+            {
+                _logger.LogInformation("Processamento Iniciado em: {time}", DateTimeOffset.UtcNow.AddHours(-3));
+
+                // Seta uma chave 
+                var _httpClient = _httpClientFactory.CreateClient(nameof(WorkerProcess));
+
+                var response = await _httpClient.GetAsync("api/v3/ticker/price");
+
+                if (!response.IsSuccessStatusCode)
+                    _logger.LogError($"ERROR CODE: {response.StatusCode}");
+                else
+                    _logger.LogInformation(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+
+                //Aguarda 5s para novo processamento.
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
